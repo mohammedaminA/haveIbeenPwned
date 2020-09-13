78590a1a5eee862f1f0d590def31ae4f935d4342
Mohammedamin Abdullah <59562112+mohammedaminA@users.noreply.github.com>
2020-09-13 16:48:54 +0300

    Update and rename main.py to password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -2,7 +2,7 @@ import requests
 import hashlib
 import sys
 
-
+# initial call to api
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
@@ -10,7 +10,7 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again. ')
     return res
 
-
+# read response 
 def get_password_leaks_count(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
@@ -18,23 +18,22 @@ def get_password_leaks_count(hashes, hash_to_check):
             return count
     return 0
 
-
+#check password on the api server. NB: K Anonymity concept applied and we are only sending the first 5 characters of the hashed password to the API
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
-    print(response)
     return get_password_leaks_count(response, tail)
 
 
 def main(args):
-    for passwords in args:
-        count = pwned_api_check((passwords))
+    for password in args:
+        count = pwned_api_check((password))
         if count:
             print(f'{passwords} was found {count} times... you should change your password')
         else:
             print(f'{passwords} was not found... carry on!')
     return "done!"
 
-
-main(sys.argv[1:])
+if __name__ = __main__:
+    main(sys.argv[1:])
